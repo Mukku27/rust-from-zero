e41afbd3415840c32d72b4de5da2d684d92280e4
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-04-04 11:01:44 +0530

    Hello , Rust!

---
+++ r0-hellorust/target/debug/deps/r0_hellorust-d6a00d2d8fc9bf3a.d
@@ -0,0 +1,5 @@
+/home/mukesh/rust/rust-from-zero/r0-hellorust/target/debug/deps/r0_hellorust-d6a00d2d8fc9bf3a: src/main.rs
+
+/home/mukesh/rust/rust-from-zero/r0-hellorust/target/debug/deps/r0_hellorust-d6a00d2d8fc9bf3a.d: src/main.rs
+
+src/main.rs:
---
+++ r0-hellorust/target/debug/r0-hellorust.d
@@ -0,0 +1,1 @@
+/home/mukesh/rust/rust-from-zero/r0-hellorust/target/debug/r0-hellorust: /home/mukesh/rust/rust-from-zero/r0-hellorust/src/main.rs
