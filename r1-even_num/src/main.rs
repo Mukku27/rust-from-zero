1b6b478662b878f8c6b706131fe1254e3330c600
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-05-24 15:33:20 +0530

    is_even function

--- r1-even_num/src/main.rs
+++ r1-even_num/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     let x:u8=12;
+    println!("{}",is_even(x.into()));
     if x%2==0 
      { println!("{} is  even number",x); }
+}
+fn is_even(num:i32)->bool {
+    if num%2==0{ 
+     return true ;}
+    return false; 
+    
 }
