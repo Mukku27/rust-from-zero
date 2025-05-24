b396fbfc05a3b1f337c612dc5dd3394746de8542
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-05-24 16:22:31 +0530

    string

--- r1-strings/src/main.rs
+++ r1-strings/src/main.rs
@@ -3,5 +3,5 @@ let my_string=String::from("I am Iron Man");
 println!("The number of characters in the string are {}",string_length(&my_string));
 }
 fn string_length(s: &str)-> usize{
-   return s.chars().count();
+  return s.chars().count();
 }
