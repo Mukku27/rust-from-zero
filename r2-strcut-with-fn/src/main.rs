3a6d50c5667c0623af71c4be92c59d7b6e06b6cd
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-05-30 17:13:32 +0530

    impl rec perimeter function

--- r2-strcut-with-fn/src/main.rs
+++ r2-strcut-with-fn/src/main.rs
@@ -7,11 +7,16 @@ impl Rect{
     {
         return self.width*self.length;
     }
+    fn perimeter(&self)->u32
+    {
+        return 2*(self.width+self.length);
+    }
 }
 fn main() {
    let rec=Rect{
     length:30,
     width:40,
    };
-   println!("The total area of the Rectangle = {}",rec.area())
+   println!("The total area of the Rectangle = {}",rec.area());
+   println!("The total perimeter of the Rectangle = {}",rec.perimeter());
 }
