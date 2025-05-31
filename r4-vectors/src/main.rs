3dd165ef06da528abe12d23d61278236ef56ba9d
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-05-31 13:03:55 +0530

    vectors as function arguements

--- r4-vectors/src/main.rs
+++ r4-vectors/src/main.rs
@@ -4,7 +4,14 @@ fn main(){
     vec.push(1);
     vec.push(2);
     vec.push(3);
+    vec.push(4);
+    vec.push(5);
+    vec.push(6);
     println!("{:?}",vec);
+    println!("The even nuumbers in vec :{:?}",is_even(vec.clone()));
+    println!("The odd nuumbers in vec :{:?}",is_odd(vec.clone()));
+    
+
 
 
     //method-1 for string vectors 
@@ -21,7 +28,27 @@ fn main(){
     vec_str.push(String::from("hello rust"));
     println!("Owned String vector: {:?}", vec_str);
 
-
 }
 
+fn is_even(vec: Vec<i32>)->Vec<i32> {
+    let mut new_vec=Vec::new();
+    for val in vec {
+        if val%2==0{
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec;
+}
+
+fn is_odd(vec: Vec<i32>)->Vec<i32> {
+    let mut new_vec=Vec::new();
+    for val in vec {
+        if val%2!=0{
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec;
+}
 
