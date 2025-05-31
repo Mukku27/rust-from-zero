384789c34ead30239ff61969af79a5757c5b5a8c
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-05-31 11:08:46 +0530

    ownership in rust

---
+++ r3-ownership/target/debug/deps/r3_ownership-12c63933ef1be1f0.d
@@ -0,0 +1,5 @@
+/home/mukesh/rust/rust-from-zero/r3-ownership/target/debug/deps/r3_ownership-12c63933ef1be1f0: src/main.rs
+
+/home/mukesh/rust/rust-from-zero/r3-ownership/target/debug/deps/r3_ownership-12c63933ef1be1f0.d: src/main.rs
+
+src/main.rs:
---
+++ r3-ownership/target/debug/r3-ownership.d
@@ -0,0 +1,1 @@
+/home/mukesh/rust/rust-from-zero/r3-ownership/target/debug/r3-ownership: /home/mukesh/rust/rust-from-zero/r3-ownership/src/main.rs
