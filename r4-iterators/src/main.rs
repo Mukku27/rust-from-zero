99877c8e7a6b30500f65d4d7c44909badc693c97
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-06-03 11:16:07 +0530

    iterating after creating an Iterator

--- r4-iterators/src/main.rs
+++ r4-iterators/src/main.rs
@@ -6,4 +6,13 @@ fn main() {
    for num in vec_num{
     println!("{}",num);
    }
+
+   //2.iterating after creating an Iterator
+   println!("iterating after creating an Iterator");
+   let num=vec![1,2,3,4,5,6,7];
+   let iter=num.iter();
+
+   for value in iter {
+    println!("{}",value);
+   }
 }
