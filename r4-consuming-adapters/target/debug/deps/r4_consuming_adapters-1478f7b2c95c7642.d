c16c4c453809ea40e163ee8d9614ff3967b682ab
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-06-03 12:19:37 +0530

    adapter -> adaptor renamed

--- r4-consuming-adapters/target/debug/deps/r4_consuming_adapters-1478f7b2c95c7642.d
+++
@@ -1,5 +0,0 @@
-/home/mukesh/rust/rust-from-zero/r4-consuming-adapters/target/debug/deps/r4_consuming_adapters-1478f7b2c95c7642: src/main.rs
-
-/home/mukesh/rust/rust-from-zero/r4-consuming-adapters/target/debug/deps/r4_consuming_adapters-1478f7b2c95c7642.d: src/main.rs
-
-src/main.rs:
--- r4-consuming-adapters/target/debug/r4-consuming-adapters.d
+++
@@ -1,1 +0,0 @@
-/home/mukesh/rust/rust-from-zero/r4-consuming-adapters/target/debug/r4-consuming-adapters: /home/mukesh/rust/rust-from-zero/r4-consuming-adapters/src/main.rs
