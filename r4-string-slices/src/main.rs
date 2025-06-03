05b9e67d79e361d44445ded3d198bfe4a9fa72f2
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-06-03 15:26:32 +0530

    common types of strings

--- r4-string-slices/src/main.rs
+++ r4-string-slices/src/main.rs
@@ -4,4 +4,14 @@ fn main() {
   let second_name=&name[6..11];//string slice &str
   println!("{}",first_name);
   println!("{}",second_name);
+
+
+  //most common  types of string
+
+  let name = String::from("hello world"); // String type
+  let string_slice = &name; // Has a `view` into the original string/is a reference
+  let string_literal = "hello"; // literal is also an &str but it points directly to an address in the binary
+
+  println!("{}",string_slice);
+  println!("{}",string_literal);
 }
